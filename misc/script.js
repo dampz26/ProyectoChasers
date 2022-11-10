2dab00d3b2b764cbae16a45618aa8365ad0a7db4
dampz <daloma1126@gmail.com>
2022-11-09 23:09:58 -0500

    Se trabajó en diseño responsive del nav bar, se agregó archivo JS que no sirve aún y se modificaron archivos css para diseño responsive

---
+++ misc/script.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector('toggle-menu');
+const navbarLinks = document.querySelector('links-menu');
+
+
+toggleMenu.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
